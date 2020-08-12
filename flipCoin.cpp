9d55a5a362dcd99da72d541f5382de3d2ebda020
“Shubham” <shubhampattar2@gmail.com>
2020-08-13 00:35:29 +0530

    ADDED Extended To Code For Flip Coin Simulation.

--- flipCoin.cpp
+++ flipCoin.cpp
@@ -6,14 +6,33 @@ using namespace std;
 
 int main() {
     srand(time(0));
-    int coin = (rand() % 2 + 1);
-    if (coin == 1)
+    int headCounter = 0, tailCounter = 0;
+    while (true)
     {
-        cout << "Head";
-    }
-    else
-    {
-        cout << "Tail";
+        int coin = (rand() % 2 + 1);
+        if (coin == 1)
+        {
+            cout << "Head\n";
+            headCounter++;
+            if (headCounter == 11)
+            {
+                cout << "Head Count: " << headCounter << endl;
+                cout << "Head Won\n";
+                break; 
+            }
+            
+        }
+        else
+        {
+            cout << "Tail\n";
+            tailCounter++;
+            if (tailCounter == 11)
+            {
+                cout << "Tail Count: " << tailCounter << endl;
+                cout << "Tail Won\n";
+                break; 
+            }
+        }
     }
     return 0;
 }
