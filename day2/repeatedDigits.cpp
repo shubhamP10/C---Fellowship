8d35d988e82634f7648ce7aece89359a12c3bf3c
“Shubham” <shubhampattar2@gmail.com>
2020-08-14 13:58:44 +0530

    ADDED Day2 Practice Problem 1.

---
+++ day2/repeatedDigits.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int main(){
+    int number = 0;
+    int arr[20];
+
+    for (int i = 1; i <= 100; i++)  
+    {
+        if (i % 11 == 0)
+        {
+            arr[number] = i;
+            number++; 
+        }
+    }
+
+    cout << "Repeated Digits Between 1 - 100 are :" << endl;
+
+    for (int i = 0; i < number; i++)
+    {
+        cout << arr[i] << endl;
+    }
+      
+    return 0;
+}
