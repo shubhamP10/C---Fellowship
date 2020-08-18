1c0e4ce98cd3b7babb2241e394b6e3ed6a7bc978
“Shubham” <shubhampattar2@gmail.com>
2020-08-18 10:20:05 +0530

    ADDED Insertion Sort Code.

---
+++ day4/insertionSort.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+
+using namespace std;
+
+void insertionSort(int arr[], int size);
+
+void printArray(int arr[], int size);
+
+int main(){
+    int size;
+    cout << "Enter How Many Numbers to sort?" << endl;
+    cin >> size;
+    int arr[size];
+    cout << "Enter The Numbers: " << endl;
+    for (int i = 0; i < size; i++){
+        cin >> arr[i];
+    }
+
+    cout << "Before Sorting: " << endl;
+    printArray(arr, size);
+
+    cout << "\nAfter Sorting: " << endl;
+    insertionSort(arr, size);
+    
+    return 0;
+}
+
+void printArray(int arr[], int size){
+    for (int i = 0; i < size; i++){
+        cout << arr[i] << " ";
+    }  
+}
+
+void insertionSort(int arr[], int size){
+    int i , j, key;
+    for (int i = 1; i < size; i++){
+        key = arr[i];
+        j = i - 1;
+        while (j >= 0 && arr[j] > key){
+            arr[j+1] = arr[j];
+            j = j - 1;
+        }
+        arr[j+1] = key;
+    }
+    printArray(arr, size);
+}
